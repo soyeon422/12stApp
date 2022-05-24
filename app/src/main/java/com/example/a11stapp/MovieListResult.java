8f8cf53da99fa634405b23bd8f61a1bcb273937a
soyeon422 <soyeon4244@naver.com>
2022-05-24 16:18:01 +0900

    영화정보의 수 추가

---
+++ app/src/main/java/com/example/a11stapp/MovieListResult.java
@@ -0,0 +1,11 @@
+package com.example.a11stapp;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+}
